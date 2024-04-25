4a7d155a2c714e0886ace081b9a8c15990eabd21
Cleber Henrique <cleberschiavon@outlook.com>
2024-04-25 13:50:23 -0300

    feat(app-module): cvreating http logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import { ScraperService } from "services/scraper/scraper.service";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { BrowserService } from "services/browser/browser.service";
+import { HTTPLoggerInterceptor } from "./middlewares/http.logger.middleware";
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController, ScraperController],
   providers: [AppService, ScraperService, BrowserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HTTPLoggerInterceptor).forRoutes("*");
+  }
+}
